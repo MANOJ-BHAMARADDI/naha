e5c1d4326ce63e86be55bb5f62adb996c2ae2db2
Manoj Bhamaraddi <manojbhamaraddi@gmail.com>
2025-03-12 21:14:16 +0530

    Configured CORS for frontend

--- naha-backend/server.js
+++ naha-backend/server.js
@@ -21,7 +21,9 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://naha-frontend.vercel.app", credentials: true }));
+
 
 app.use(cookieParser());
 
